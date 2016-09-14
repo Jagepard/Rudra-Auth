5c23ba8ad8729b853bbb8f0c9489f3da85bed804
Jagepard <dankorot@gmail.com>
2016-09-14 11:20:52 +0300

    first commit

---
+++ src/Auth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Rudra;
+
+
+    /**
+     * Date: 14.09.16
+     * Time: 11:16
+     * @author    : Korotkov Danila <dankorot@gmail.com>
+     * @copyright Copyright (c) 2016, Korotkov Danila
+     * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+     */
+
+/**
+ * Class Auth
+ * @package Rudra
+ */
+class Auth
+{
+
+}
