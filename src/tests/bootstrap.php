5ae37cf4266c4bafd07e8efd028f9c332866c192
Jagepard <dankorot@gmail.com>
2017-03-23 19:30:30 +0300

    Добавляет bootstrap  для phpunit

---
+++ src/tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Date: 22.03.17
+ * Time: 14:25
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+require_once 'stub/Redirect.php';
+require_once dirname(__DIR__) . '/Auth.php';
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
