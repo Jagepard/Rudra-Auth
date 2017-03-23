3545d3c378b3bc9cb6f3ce60a7d826a2c2b477bd
Jagepard <dankorot@gmail.com>
2017-03-23 19:31:56 +0300

    Добавляет stub/Redirect

---
+++ src/tests/stub/Redirect.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Date: 23.03.17
+ * Time: 14:31
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2016, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace stub;
+
+
+class Redirect
+{
+
+    /**
+     * @param array ...$params
+     *
+     * @return null
+     */
+    public function run(...$params)
+    {
+        return null;
+    }
+}
