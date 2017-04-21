8b6e77d7970bece38390ecdce9328b8a8cf47310
Jagepard <dankorot@gmail.com>
2017-04-21 13:28:48 +0300

    Переносит /vendor/autoload.php

--- src/tests/bootstrap.php
+++ src/tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 define('DEV', 'test');
 
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/Auth.php';
 require_once dirname(__DIR__) . '/AuthTrait.php';
-require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 require_once 'stub/StubClass.php';
