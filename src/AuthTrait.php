f41bab780b48265aeadb6b545aed72e085f64885
Jagepard <dankorot@gmail.com>
2018-05-20 04:10:32 +0300

    Updated

--- src/AuthTrait.php
+++ src/AuthTrait.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /**
  * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2016, Korotkov Danila
+ * @copyright Copyright (c) 2018, Korotkov Danila
  * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
  */
 
