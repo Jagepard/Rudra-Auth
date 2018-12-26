4878d56bcba048eb40f713e464d064e13339a6d0
Jagepard <dankorot@gmail.com>
2018-12-26 17:37:15 +0300

    loginRedirectWithFlash updated

--- src/AuthBase.php
+++ src/AuthBase.php
@@ -100,7 +100,7 @@ class AuthBase
      */
     protected function loginRedirectWithFlash(string $notice): void
     {
-        $this->container->setSession('alert',  $notice, 'main');
+        $this->container->setSession('alert',  $notice, 'error');
         $this->container->get('redirect')->run('stargate');
     }
 
