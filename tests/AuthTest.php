57f4e969704c55ec9d67a9c54e3ad8266ed40c4f
Jagepard <dankorot@gmail.com>
2020-09-07 17:38:16 +0300

    update tests

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -21,6 +21,7 @@ class AuthTest extends PHPUnit_Framework_TestCase
 
     protected function setUp(): void
     {
+        $_SESSION = [];
         Application::run()->request()->server()->set(["REMOTE_ADDR" => "127.0.0.1"]);
         Application::run()->request()->server()->set(["HTTP_USER_AGENT" => "Mozilla"]);
         $this->stubClass = new StubClass();
@@ -29,17 +30,17 @@ class AuthTest extends PHPUnit_Framework_TestCase
     public function testRegularAccess()
     {
         Application::run()->session()->set(["token", "token"]);
-        $this->assertTrue($this->stubClass()->auth());
+        $this->assertTrue($this->stubClass->auth());
 
         Application::run()->session()->unset("token");
-        $this->assertNull($this->stubClass()->auth('someToken'));
+        $this->assertNull($this->stubClass->auth('someToken'));
     }
 
     public function testUserAccess(): void
     {
         /* User Access */
         Application::run()->session()->set(["token", "userIdToken"]);
-        $this->assertTrue($this->stubClass()->auth("userIdToken"));
+        $this->assertTrue($this->stubClass->auth("userIdToken"));
 
         Application::run()->session()->unset("token");
         $this->assertFalse(Application::run()->objects()->get("auth")->access("userIdToken"));
@@ -48,15 +49,15 @@ class AuthTest extends PHPUnit_Framework_TestCase
 
     public function testCheck(): void
     {
-        $this->assertNull($this->stubClass()->updateSessionIfSetRememberMe());
+        $this->assertNull($this->stubClass->updateSessionIfSetRememberMe());
 
         Application::run()->session()->set(["token", "userIdToken"]);
-        $this->stubClass()->updateSessionIfSetRememberMe();
+        $this->stubClass->updateSessionIfSetRememberMe();
         $this->assertEquals("userIdToken", Application::run()->session()->get("token"));
 
         Application::run()->cookie()->set(["RudraPermit", "userIdToken"]);
         Application::run()->cookie()->set(["RudraToken", "userIdToken"]);
-        $this->stubClass()->updateSessionIfSetRememberMe();
+        $this->stubClass->updateSessionIfSetRememberMe();
 
         $this->assertEquals(Application::run()->cookie()->get("RudraToken"), Application::run()->session()->get("token"));
     }
@@ -64,13 +65,13 @@ class AuthTest extends PHPUnit_Framework_TestCase
     public function testLogin(): void
     {
         $this->assertNull(
-            $this->stubClass()->login("password", [
+            $this->stubClass->login("password", [
                 "email"    => "",
                 "password" => password_hash("password", PASSWORD_BCRYPT, ['cost' => 10])
             ]));
 
         $this->assertNull(
-            $this->stubClass()->login('wrong', [
+            $this->stubClass->login('wrong', [
                 "email"    => '',
                 "password" => password_hash("password", PASSWORD_BCRYPT, ['cost' => 10])
             ]));
@@ -78,27 +79,27 @@ class AuthTest extends PHPUnit_Framework_TestCase
 
     public function testLogout(): void
     {
-        unset($_COOKIE['RudraPermit']);
-        $this->assertNull($this->stubClass()->logout());
-        $this->assertFalse(Application::run()->hasSession("token"));
+//        unset($_COOKIE['RudraPermit']);
+        $this->assertNull($this->stubClass->logout());
+        $this->assertFalse(Application::run()->session()->has("token"));
     }
 
     public function testRole(): void
     {
-        $this->assertTrue($this->stubClass()->role('admin', 'C'));
-        $this->assertTrue($this->stubClass()->role('admin', 'U'));
-        $this->assertTrue($this->stubClass()->role('admin', 'D'));
+        $this->assertTrue($this->stubClass->role('admin', 'C'));
+        $this->assertTrue($this->stubClass->role('admin', 'U'));
+        $this->assertTrue($this->stubClass->role('admin', 'D'));
 
-        $this->assertFalse($this->stubClass()->role('editor', 'D'));
-        $this->assertFalse($this->stubClass()->role('editor', 'D', ''));
-        $this->assertTrue($this->stubClass()->role('editor', 'C'));
-        $this->assertTrue($this->stubClass()->role('editor', 'U'));
+        $this->assertFalse($this->stubClass->role('editor', 'D'));
+        $this->assertFalse($this->stubClass->role('editor', 'D', ''));
+        $this->assertTrue($this->stubClass->role('editor', 'C'));
+        $this->assertTrue($this->stubClass->role('editor', 'U'));
 
-        $this->assertFalse($this->stubClass()->role('user', 'D'));
-        $this->assertFalse($this->stubClass()->role('user', 'U'));
-        $this->assertFalse($this->stubClass()->role('user', 'D', ''));
-        $this->assertFalse($this->stubClass()->role('user', 'U', ''));
-        $this->assertTrue($this->stubClass()->role('user', 'C'));
+        $this->assertFalse($this->stubClass->role('user', 'D'));
+        $this->assertFalse($this->stubClass->role('user', 'U'));
+        $this->assertFalse($this->stubClass->role('user', 'D', ''));
+        $this->assertFalse($this->stubClass->role('user', 'U', ''));
+        $this->assertTrue($this->stubClass->role('user', 'C'));
     }
 
     /**
@@ -107,32 +108,24 @@ class AuthTest extends PHPUnit_Framework_TestCase
     public function testJsonResponse()
     {
         /* Regular Access */
-        Application::run()->setSession("token", "token");
-        $this->assertTrue(Application::run()->get('auth')->access(null, 'API'));
+        Application::run()->session()->set(["token", "token"]);
+        $this->assertTrue(Application::run()->objects()->get("auth")->access(null, "API"));
 
-        $this->stubClass()->logout();
-        $this->assertNull(Application::run()->get('auth')->access(null, 'API'));
+        $this->stubClass->logout();
+        $this->assertNull(Application::run()->objects()->get("auth")->access(null, "API"));
     }
 
     public function testHash()
     {
         $password = "password";
-        $hash     = $this->stubClass()->bcrypt($password);
+        $hash     = $this->stubClass->bcrypt($password);
 
         $this->assertTrue(password_verify($password, $hash));
     }
 
     public function testUserToken()
     {
-        Application::run()->setSession("token", 'someToken');
-        $this->assertEquals($this->stubClass()->userToken(), Application::run()->getSession("token"));
-    }
-
-    /**
-     * @return StubClass
-     */
-    public function stubClass(): StubClass
-    {
-        return $this->stubClass;
+        Application::run()->session()->set(["token", "someToken"]);
+        $this->assertEquals($this->stubClass->userToken(), Application::run()->session()->get("token"));
     }
 }
