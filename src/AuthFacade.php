55c4e474898e7fc2113a78d6ed11d34658d84877
Jagepard <dankorot@gmail.com>
2020-10-06 17:05:08 +0300

    add docs

--- src/AuthFacade.php
+++ src/AuthFacade.php
@@ -9,6 +9,16 @@ namespace Rudra\Auth;
 
 use Rudra\Container\Traits\FacadeTrait;
 
+/**
+ * @method static login(string $password, array $userData, string $redirect = "admin", string $notice = "Please enter correct information")
+ * @method static void logout(string $redirect = "")
+ * @method static access(string $token = null, string $redirect = null)
+ * @method static role(string $role, string $privilege, string $redirect = null)
+ * @method static void updateSessionIfSetRememberMe($redirect = "login")
+ * @method static string bcrypt(string $password, int $cost = 10)
+ *
+ * @see Auth
+ */
 final class AuthFacade
 {
     use FacadeTrait;
