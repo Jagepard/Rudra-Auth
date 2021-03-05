7a5db6a3c6925301a4680c5408896ba071488ff9
Jagepard <dankorot@gmail.com>
2021-03-05 14:54:31 +0300

    refactored

--- src/AuthInterface.php
+++ src/AuthInterface.php
@@ -9,10 +9,61 @@ namespace Rudra\Auth;
 
 interface AuthInterface
 {
-    public function login(string $password, \stdClass $user, string $redirect = "admin", string $notice = "");
-    public function updateSessionIfSetRememberMe($redirect): void;
-    public function access(string $token = null, string $redirect = null);
-    public function logout(string $redirect = ""): void;
-    public function role(string $role, string $privilege, string $redirect = null);
+    /**
+     * @param \stdClass $user
+     * @param string $password
+     * @param string $redirect
+     * @param string $notice
+     * @return callable
+     *
+     * Authentication
+     * --------------
+     * Аутентификация
+     */
+    public function authentication(\stdClass $user, string $password, string $redirect = "", string $notice = "");
+
+    /**
+     * @param string $redirect
+     *
+     * Exit authentication session
+     * ---------------------------
+     * Выйти из сеанса аутентификации
+     */
+    public function exitAuthenticationSession(string $redirect = ""): void;
+
+    /**
+     * @param string|null $token
+     * @param string|null $redirect
+     * @return bool|callable
+     *
+     * Authorization
+     * Providing access to shared or personal resources
+     * ------------------------------------------------
+     * Авторизация
+     * Предоставление доступа к общим или личным ресурсам
+     */
+    public function authorization(string $token = null, string $redirect = null);
+
+    /**
+     * @param string $role
+     * @param string $privilege
+     * @param string|null $redirect
+     * @return bool
+     *
+     * Role based access
+     * -----------------
+     * Доступ на основе ролей
+     */
+    public function roleBasedAccess(string $role, string $privilege, string $redirect = null);
+
+    /**
+     * @param string $password
+     * @param int $cost
+     * @return string
+     *
+     * Creates a password hash
+     * -----------------------
+     * Создаёт хеш пароля
+     */
     public function bcrypt(string $password, int $cost = 10): string;
 }
