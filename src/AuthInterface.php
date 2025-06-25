8a5c8861d1d7f0df934767fe3c934524f51f1a3f
Jagepard <jagepard@yandex.ru>
2025-06-25 14:46:29 +0300

    add minimum docs

--- src/AuthInterface.php
+++ src/AuthInterface.php
@@ -11,9 +11,40 @@ namespace Rudra\Auth;
 
 interface AuthInterface
 {
+    /**
+     * @param  array  $user
+     * @param  string $password
+     * @param  array  $redirect
+     * @param  array  $notice
+     * @return void
+     */
     public function authentication(array $user, string $password, array $redirect = ['admin', 'login'], array $notice = ["error" => "Wrong access data"]);
+
+    /**
+     * @param  string $redirect
+     * @return void
+     */
     public function logout(string $redirect = ""): void;
+
+    /**
+     * @param  string|null $token
+     * @param  string|null $redirect
+     * @return void
+     */
     public function authorization(string $token = null, string $redirect = null);
+
+    /**
+     * @param  string      $role
+     * @param  string      $privilege
+     * @param  string|null $redirect
+     * @return void
+     */
     public function roleBasedAccess(string $role, string $privilege, string $redirect = null);
+
+    /**
+     * @param  string  $password
+     * @param  integer $cost
+     * @return string
+     */
     public function bcrypt(string $password, int $cost = 10): string;
 }
